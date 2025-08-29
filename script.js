602e4a66a4314eb390840137a4946522cb0f1f59
arafat100 <arafatr360@gmail.com>
2025-08-29 18:51:35 +0600

    call&copy

---
+++ script.js
@@ -0,0 +1,53 @@
+// heart
+let heartCount = 0;
+const heartClick = document.getElementsByClassName("heart-click");
+for (let heart of heartClick) {
+    heart.addEventListener("click", function (e) {
+        e.preventDefault();
+        heartCount++;
+        document.getElementById("heart-sum").innerText = heartCount;
+
+    })
+}
+
+
+// call
+const callClick = document.getElementsByClassName("call-click");
+
+for (const call of callClick) {
+    call.addEventListener("click", function (e) {
+        e.preventDefault();
+        const coinNumber = 20;
+        const coinTotal = parseInt(document.getElementById("coin").innerText);
+
+        if (coinTotal >= coinNumber) {
+            alert("calling national emergency service 999")
+            const coinFinal = coinTotal - coinNumber;
+            document.getElementById("coin").innerText = coinFinal;
+            return
+        }
+        else {
+            alert("insufficient coins.you need at least 20 coins to call")
+        }
+    });
+}
+
+
+// copy
+const copyClick = document.getElementsByClassName("copy-click");
+for (const copy of copyClick) {
+    copy.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        const coinNumber = 20;
+        const coinTotal = parseInt(document.getElementById("coin").innerText);
+
+        const coinFinal = coinTotal + coinNumber;
+        document.getElementById("coin").innerText = coinFinal;
+    });
+}
+
+
+
+
+
